c4fb642218d17a155b87c78df5f3a3beb4859ded
kazbekovruslan <chartusss@yandex.ru>
2023-09-21 00:26:06 +0300

    finish main

--- Homework1/MatrixMultiplication/Program.cs
+++ Homework1/MatrixMultiplication/Program.cs
@@ -2,8 +2,32 @@
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        var matrix = new Matrix("file");
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Incorrect amount of arguments. Enter 'dotnet run [path to 1 matrix] [path to 2 matrix]'.");
+        }
+        else
+        {
+            try
+            {
+                var firstMatrix = new Matrix(args[0]);
+                var secondMatrix = new Matrix(args[1]);
+
+                var commonMultiply = Matrix.Multiply(firstMatrix, secondMatrix);
+                commonMultiply.WriteInFile("result.txt");
+
+                var parallelMultiply = Matrix.ParallelMultiply(firstMatrix, secondMatrix);
+                parallelMultiply.WriteInFile("result_parallel.txt");
+
+                Console.WriteLine("Done! The result is written to files 'result.txt' and 'result_parallel.txt'.");
+            }
+            catch (Exception ex) when (ex is ArgumentException ||
+                                       ex is FileNotFoundException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
