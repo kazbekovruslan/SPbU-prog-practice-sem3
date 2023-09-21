cc8b601f457ebce8b4a23e51a2f7003fed9bdc5a
kazbekovruslan <chartusss@yandex.ru>
2023-09-21 19:22:21 +0300

    add measurement of speed of multiplication

--- Homework1/MatrixMultiplication/MatrixMultiplication/Program.cs
+++ Homework1/MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -4,29 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length != 2)
+        if (args.Length != 1 && args.Length != 2)
         {
-            Console.WriteLine("Incorrect amount of arguments. Enter 'dotnet run [path to 1 matrix] [path to 2 matrix]'.");
+            Console.WriteLine("Incorrect amount of arguments. Enter 'dotnet run --help' to see available arguments.");
         }
         else
         {
-            try
+            if (args.Length == 1)
             {
-                var firstMatrix = new Matrix(args[0]);
-                var secondMatrix = new Matrix(args[1]);
-
-                var commonMultiply = Matrix.Multiply(firstMatrix, secondMatrix);
-                commonMultiply.WriteInFile("result.txt");
-
-                var parallelMultiply = Matrix.ParallelMultiply(firstMatrix, secondMatrix);
-                parallelMultiply.WriteInFile("result_parallel.txt");
-
-                Console.WriteLine("Done! The result is written to files 'result.txt' and 'result_parallel.txt'.");
+                if (args[0] == "help")
+                {
+                    // 4to-to
+                }
+                else if (args[0] == "benchmark")
+                {
+                    Console.WriteLine("Running benchmark...");
+                    MatrixBenchmark.WritingResultsOfBenchmarkingToFile();
+                    Console.WriteLine("Done!");
+                }
             }
-            catch (Exception ex) when (ex is ArgumentException ||
-                                       ex is FileNotFoundException)
+            else
             {
-                Console.WriteLine(ex.Message);
+                try
+                {
+                    var firstMatrix = new Matrix(args[0]);
+                    var secondMatrix = new Matrix(args[1]);
+
+                    var commonMultiply = Matrix.Multiply(firstMatrix, secondMatrix);
+                    commonMultiply.WriteInFile("result.txt");
+
+                    var parallelMultiply = Matrix.ParallelMultiply(firstMatrix, secondMatrix);
+                    parallelMultiply.WriteInFile("result_parallel.txt");
+
+                    Console.WriteLine("Done! The result is written to files 'result.txt' and 'result_parallel.txt'.");
+                }
+                catch (Exception ex) when (ex is ArgumentException ||
+                                           ex is FileNotFoundException)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
     }
