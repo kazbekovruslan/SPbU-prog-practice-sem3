9959081b572a070a9a1c122c70b0614e97361c27
kazbekovruslan <chartusss@yandex.ru>
2023-09-21 11:11:00 +0300

    rename folder

---
+++ Homework1/MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MatrixMultiplication;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Incorrect amount of arguments. Enter 'dotnet run [path to 1 matrix] [path to 2 matrix]'.");
+        }
+        else
+        {
+            try
+            {
+                var firstMatrix = new Matrix(args[0]);
+                var secondMatrix = new Matrix(args[1]);
+
+                var commonMultiply = Matrix.Multiply(firstMatrix, secondMatrix);
+                commonMultiply.WriteInFile("result.txt");
+
+                var parallelMultiply = Matrix.ParallelMultiply(firstMatrix, secondMatrix);
+                parallelMultiply.WriteInFile("result_parallel.txt");
+
+                Console.WriteLine("Done! The result is written to files 'result.txt' and 'result_parallel.txt'.");
+            }
+            catch (Exception ex) when (ex is ArgumentException ||
+                                       ex is FileNotFoundException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
