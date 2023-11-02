76377149d0bff47e86b5fbe50684e3c087ad9759
kazbekovruslan <chartusss@yandex.ru>
2023-11-02 23:35:19 +0300

    forgot about xml docs :)

--- test1/test1/Client.cs
+++ test1/test1/Client.cs
@@ -2,15 +2,27 @@ namespace Chat;
 
 using System.Net.Sockets;
 
+/// <summary>
+/// The Client class is responsible for establishing a connection to a remote server and managing communication.
+/// </summary>
 public class Client
 {
     private readonly TcpClient client;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Client"/> class with the specified IP address and port number.
+    /// </summary>
+    /// <param name="ip">The IP address of the remote server.</param>
+    /// <param name="port">The port number to connect to on the remote server.</param>
     public Client(string ip, int port)
     {
         client = new TcpClient(ip, port);
     }
 
+    /// <summary>
+    /// Starts the client and establishes a connection to the remote server.
+    /// </summary>
+    /// <returns>A Task representing the asynchronous operation.</returns>
     public async Task Run()
     {
         using var stream = client.GetStream();
--- test1/test1/Server.cs
+++ test1/test1/Server.cs
@@ -2,15 +2,25 @@ namespace Chat;
 
 using System.Net.Sockets;
 
+/// <summary>
+/// Represents a TCP server for communication with clients.
+/// </summary>
 public class Server
 {
     private readonly TcpListener listener;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Server"/> class with the specified port numbers.
+    /// </summary>
+    /// <param name="port">The port on which the server will listen for incoming connections.</param>
     public Server(int port)
     {
         listener = new TcpListener(System.Net.IPAddress.Any, port);
     }
 
+    /// <summary>
+    /// Starts the server and listens for incoming connections.
+    /// </summary>
     public async Task Run()
     {
         listener.Start();
