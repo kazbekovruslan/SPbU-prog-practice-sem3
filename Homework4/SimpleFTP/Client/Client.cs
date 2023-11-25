9df44e61b9798b7c8d9c50e00600c6713016867e
kazbekovruslan <chartusss@yandex.ru>
2023-11-25 20:23:37 +0300

    delete unnecessary comment

--- Homework4/SimpleFTP/Client/Client.cs
+++ Homework4/SimpleFTP/Client/Client.cs
@@ -24,8 +24,6 @@ public class Client
         using var reader = new StreamReader(stream);
 
         await writer.WriteLineAsync($"1 {path}");
-        // var response = await reader.ReadLineAsync();
-        // ReadLineAsync нельзя, потому что в сообщении могут быть \n
         var response = await reader.ReadToEndAsync();
 
         return response;
