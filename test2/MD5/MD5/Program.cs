0c2b0062de44e4af05d0bf0df2a01311884e82a5
kazbekovruslan <chartusss@yandex.ru>
2023-11-30 20:04:59 +0300

    fix of main (top level statements doesnt work :( )

--- test2/MD5/MD5/Program.cs
+++ test2/MD5/MD5/Program.cs
@@ -1,45 +1,48 @@
 ﻿namespace MD5;
 
-
-static void PrintHelp()
+class Program
 {
-    Console.WriteLine("""
+    static void PrintHelp()
+    {
+        Console.WriteLine("""
 
-    This is the application that computes check-sum file system's directory.
-    Usage: dotnet run {pathToDirectory}
+        This is the application that computes check-sum file system's directory.
+        Usage: dotnet run {pathToDirectory}
 
-    """);
+        """);
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            PrintHelp();
+            return;
+        }
+
+        try
+        {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            var checkSumSingleThread = CheckSum.ComputeCheckSum(args[0]);
+            stopwatch.Stop();
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            var checkSumMultiThread = CheckSum.ComputeCheckSumParallel(args[0]);
+            stopwatch.Stop();
+            Console.WriteLine($"Single thread: Time - {stopwatch.Elapsed.TotalSeconds}");
+            Console.WriteLine($"Multi thread: Time - {stopwatch.Elapsed.TotalSeconds}");
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.WriteLine("There is no directory or file on this path!");
+            PrintHelp();
+            return;
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("There is error with reading from file!");
+        }
+    }
 }
-
-
-if (args.Length != 1)
-{
-    PrintHelp();
-    return;
-}
-
-try
-{
-    var stopwatch = new System.Diagnostics.Stopwatch();
-    stopwatch.Start();
-    var checkSumSingleThread = CheckSum.ComputeCheckSum(args[0]);
-    stopwatch.Stop();
-
-    stopwatch.Reset();
-    stopwatch.Start();
-    var checkSumMultiThread = CheckSum.ComputeCheckSumParallel(args[0]);
-    stopwatch.Stop();
-    Console.WriteLine($"Single thread: Time - {stopwatch.Elapsed.TotalSeconds}");
-    Console.WriteLine($"Multi thread: Time - {stopwatch.Elapsed.TotalSeconds}");
-}
-catch (DirectoryNotFoundException)
-{
-    Console.WriteLine("There is no directory or file on this path!");
-    PrintHelp();
-    return;
-}
-catch (IOException)
-{
-    Console.WriteLine("There is error with reading from file!");
-}
-
