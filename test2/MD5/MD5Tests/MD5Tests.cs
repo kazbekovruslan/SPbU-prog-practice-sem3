f6b09d7147cc7be4fa0a905fbe81402b77625d4b
kazbekovruslan <chartusss@yandex.ru>
2023-11-30 20:14:04 +0300

    add tests

--- test2/MD5/MD5Tests/MD5Tests.cs
+++ test2/MD5/MD5Tests/MD5Tests.cs
@@ -1,15 +1,33 @@
 namespace MD5Tests;
 
+using MD5;
+
 public class Tests
 {
-    [SetUp]
-    public void Setup()
+    [Test]
+    public void CheckSumShouldBeEqualInTwoComputes()
     {
+        var path = "../../../../MD5";
+        var checkSum1 = CheckSum.ComputeCheckSum(path);
+        var checkSum2 = CheckSum.ComputeCheckSum(path);
+        Assert.That(checkSum1, Is.EqualTo(checkSum2));
     }
 
     [Test]
-    public void Test1()
+    public void CheckSumParallelShouldBeEqualInTwoComputes()
     {
-        Assert.Pass();
+        var path = "../../../../MD5";
+        var checkSum1 = CheckSum.ComputeCheckSumParallel(path);
+        var checkSum2 = CheckSum.ComputeCheckSumParallel(path);
+        Assert.That(checkSum1, Is.EqualTo(checkSum2));
+    }
+
+    [Test]
+    public void CheckSumAndCheckSumParallelShouldBeEqual()
+    {
+        var path = "../../../../MD5";
+        var checkSum1 = CheckSum.ComputeCheckSum(path);
+        var checkSum2 = CheckSum.ComputeCheckSumParallel(path);
+        Assert.That(checkSum1, Is.EqualTo(checkSum2));
     }
 }
